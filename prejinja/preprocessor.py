d388b1c36bf71ec0da3592b7640f4fbcec72fee4
giacomounivr <giacomo.marchioro@univr.it>
2024-03-15 16:13:06 +0100

    Test for multiple import

--- prejinja/preprocessor.py
+++ prejinja/preprocessor.py
@@ -5,6 +5,29 @@ import json
 from .flagsAndLanguages import languagesFlags,languagesNames
 from .simplemarkdown import simpleMarkdown
 import html
+import re
+
+def extractLinks(logicRegexPattern,templateName):
+    statements = ['extends','import','include','from']
+    externalTemplates = []
+    def getLinks(logicRegexPattern,templateName):
+        with open(templateName,"r") as f:
+            logicsBlocks = re.findall(logicRegexPattern, f.read())
+            for lb in logicsBlocks:
+                lbs = lb.strip().split()
+                if lbs[0] in statements:
+                    if lbs[1].startswith('['):
+                        raise NotImplementedError("Sorry multiple includes are not supported yet!")
+                    else:
+                        detectedLink = lbs[1].replace("'","").replace('"',"")
+                        # This avoid circular imports:
+                        if detectedLink not in externalTemplates:
+                            getLinks(logicRegexPattern,detectedLink)
+                        externalTemplates.append(detectedLink)
+    getLinks(logicRegexPattern,templateName)
+    return externalTemplates
+
+
 
 def preprocess(srcDirs,
                 distDirs,
@@ -76,7 +99,7 @@ def preprocess(srcDirs,
         srcFiles += [path for i in fileFormats for path in Path(d).rglob("*."+i)]
 
     foldersMap = dict(zip(srcDirs, distDirs))
-
+    # load the translations from the json file.
     with open('translations.po.json') as f:
         data = json.load(f)
 
@@ -85,6 +108,7 @@ def preprocess(srcDirs,
     # for each template generate a new template for each language with the
     # translation from the translations file
     for template in srcFiles:
+        print("Wokring with %s" %template)
         for lang in translations:
             # we have to define a new environment for every language so we can
             # store the linked templates.
@@ -105,17 +129,36 @@ def preprocess(srcDirs,
             parts.append("-".join((lang,template.parts[-1])))
             newPath = Path(*parts)
             newPath.parent.mkdir(parents=True, exist_ok=True)
+
             try:
                 templateVars = data[str(template)]
             except KeyError as e:
                 #e.add_note('The template was not found use ')
                 s = f'Template {template} not found. Did you use prejinjaget?'
                 raise KeyError(s)
+
+            def inUse(templateVars,i,lang):
+                """Return true if the variable is in USE and not UNUSED.
+
+                If the user delete some variables from the includes files, these
+                are not deleted in the translations. Prejinja check for any
+                conflicting variables names so we have to remove the unused
+                variables.
+
+                Args:
+                    templateVars (list): The variables of the tempalte
+                    i (str): The variable name.
+                    lang (str): The language.
+
+                Returns:
+                    bool: True if the variable is in use.
+                """
+                return not templateVars[i]['texts'][lang].get('unused')
             # we get the text for the language
             if lang == "xx":
                 txt = {i:getDummyText(templateVars[i]['character_number'],i) for i in templateVars}
             else:
-                txt = {i: md(templateVars[i]['texts'][lang]['text']) for i in templateVars}
+                txt = {i: md(templateVars[i]['texts'][lang]['text']) for i in templateVars if inUse(templateVars,i,lang)}
             # the variables will always have a LANG constant with the current language
             txt["_LANG"] = lang
             otherLanguages = translations[:]
@@ -125,23 +168,34 @@ def preprocess(srcDirs,
             txt["_LANGUAGESFLAGS"] = languagesFlags
             try:
                 content = jinja2Template.render(**txt)
+                print(environment.templatesUsed)
             # in the case the template is using other templates
             except jinja2.UndefinedError as e:
+                print(environment.templatesUsed)
                 for templateUsed in environment.templatesUsed:
+                    print("Now adding: %s" %templateUsed)
                     #TODO: check if better to add ./ in get variable
-                    if templateUsed.startswith("./"):
+                    # here I check each template if I can find the template.
+                    if templateUsed.startswith("./"): # only the other templates
                         templateUsed = templateUsed[2:]
                         print(templateUsed)
                         externalTemplateVars = data[str(templateUsed)]
                         for i in externalTemplateVars:
-                            if i in  txt:
+                            if i in txt and not externalTemplateVars[i].get('unused'):
                                 raise ValueError(f"Variable {i} of {templateUsed} already defined in {template}")
                             if lang == "xx":
                                 txt[i] = getDummyText(externalTemplateVars[i]['character_number'],i)
                             else:
-                                txt[i] = externalTemplateVars[i]['texts'][lang]['text']
-
-                content = jinja2Template.render(**txt)
+                                txt[i] = md(externalTemplateVars[i]['texts'][lang]['text'])
+                    else:
+                        print("Skipped %s" %templateUsed)
+                try:
+                    content = jinja2Template.render(**txt)
+                except jinja2.exceptions.UndefinedError as e:
+                    print(txt)
+                    breakpoint()
+                    s = f'There was a missing variable. Did you use prejinjaget?'
+                    raise KeyError(s)
             with open(newPath, "w") as f:
                 f.write(content)
 
